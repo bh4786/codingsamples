9b87cb99eb2e7170f9e5ee4ed02d06d9123a1b93
bh4786 <65218767+bh4786@users.noreply.github.com>
2021-03-02 15:17:51 -0600

    Add files via upload

---
+++ Interaction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private float interactionDistance = 4f; //maximum distance player can interact with interactable objects
+    [SerializeField] private Camera cam = null;
+    [SerializeField] private LayerMask interactionLayer = new LayerMask(); //layer of interactable objects
+    void Update()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //shoot ray from center of camera
+        RaycastHit hit;
+
+        if (Input.GetButtonDown("Interact")) //if player presses interact button, raycast from cam and call interact function if object has interactable script
+        {
+            if (Physics.Raycast(ray, out hit, interactionDistance, interactionLayer))
+            {
+                hit.transform.gameObject.GetComponent<IInteractable>().Interact(cam.transform, hit.point);
+            }
+        }
+    }
+}
